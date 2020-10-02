71bc8c0d892c4b9fa044bf8e01ef3a625fcb0af1
Perry <49940223+parry1233@users.noreply.github.com>
2020-10-02 21:52:32 +0800

    Add files via upload

---
+++ NET_hw8_lottery2/NET_hw8_lottery2/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace NET_hw8_lottery2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "sphere.png");
+			this.imageList1.Images.SetKeyName(1, "sphere1.png");
+			this.imageList1.Images.SetKeyName(2, "sphere2.png");
+			this.imageList1.Images.SetKeyName(3, "sphere3.png");
+			this.imageList1.Images.SetKeyName(4, "sphere4.png");
+			this.imageList1.Images.SetKeyName(5, "sphere5.png");
+			this.imageList1.Images.SetKeyName(6, "sphere6.png");
+			this.imageList1.Images.SetKeyName(7, "sphere7.png");
+			this.imageList1.Images.SetKeyName(8, "sphere8.png");
+			this.imageList1.Images.SetKeyName(9, "sphere9.png");
+			this.imageList1.Images.SetKeyName(10, "sphere10.png");
+			this.imageList1.Images.SetKeyName(11, "sphere11.png");
+			this.imageList1.Images.SetKeyName(12, "sphere12.png");
+			this.imageList1.Images.SetKeyName(13, "sphere13.png");
+			this.imageList1.Images.SetKeyName(14, "sphere14.png");
+			this.imageList1.Images.SetKeyName(15, "sphere15.png");
+			this.imageList1.Images.SetKeyName(16, "sphere16.png");
+			this.imageList1.Images.SetKeyName(17, "sphere17.png");
+			this.imageList1.Images.SetKeyName(18, "sphere18.png");
+			this.imageList1.Images.SetKeyName(19, "sphere19.png");
+			this.imageList1.Images.SetKeyName(20, "sphere20.png");
+			this.imageList1.Images.SetKeyName(21, "sphere21.png");
+			this.imageList1.Images.SetKeyName(22, "sphere22.png");
+			this.imageList1.Images.SetKeyName(23, "sphere23.png");
+			this.imageList1.Images.SetKeyName(24, "sphere24.png");
+			this.imageList1.Images.SetKeyName(25, "sphere25.png");
+			this.imageList1.Images.SetKeyName(26, "sphere26.png");
+			this.imageList1.Images.SetKeyName(27, "sphere27.png");
+			this.imageList1.Images.SetKeyName(28, "sphere28.png");
+			this.imageList1.Images.SetKeyName(29, "sphere29.png");
+			this.imageList1.Images.SetKeyName(30, "sphere30.png");
+			this.imageList1.Images.SetKeyName(31, "sphere31.png");
+			this.imageList1.Images.SetKeyName(32, "sphere32.png");
+			this.imageList1.Images.SetKeyName(33, "sphere33.png");
+			this.imageList1.Images.SetKeyName(34, "sphere34.png");
+			this.imageList1.Images.SetKeyName(35, "sphere35.png");
+			this.imageList1.Images.SetKeyName(36, "sphere36.png");
+			this.imageList1.Images.SetKeyName(37, "sphere37.png");
+			this.imageList1.Images.SetKeyName(38, "sphere38.png");
+			this.imageList1.Images.SetKeyName(39, "sphere39.png");
+			this.imageList1.Images.SetKeyName(40, "sphere40.png");
+			this.imageList1.Images.SetKeyName(41, "sphere41.png");
+			this.imageList1.Images.SetKeyName(42, "sphere42.png");
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.pictureBox6);
+			this.panel1.Controls.Add(this.pictureBox5);
+			this.panel1.Controls.Add(this.pictureBox4);
+			this.panel1.Controls.Add(this.pictureBox3);
+			this.panel1.Controls.Add(this.pictureBox2);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(650, 123);
+			this.panel1.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(102, 102);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+			this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox2.Location = new System.Drawing.Point(111, 3);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(102, 102);
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+			this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox3.Location = new System.Drawing.Point(219, 3);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(102, 102);
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+			this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox4.Location = new System.Drawing.Point(327, 3);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(102, 102);
+			this.pictureBox4.TabIndex = 3;
+			this.pictureBox4.TabStop = false;
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+			this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox5.Location = new System.Drawing.Point(435, 3);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(102, 102);
+			this.pictureBox5.TabIndex = 4;
+			this.pictureBox5.TabStop = false;
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+			this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox6.Location = new System.Drawing.Point(543, 3);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(102, 102);
+			this.pictureBox6.TabIndex = 5;
+			this.pictureBox6.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button1.Location = new System.Drawing.Point(264, 141);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(137, 42);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "開獎";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(674, 192);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ NET_hw8_lottery2/NET_hw8_lottery2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NET_hw8_lottery2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			pictureBox1.BackgroundImage = imageList1.Images[0];
+			pictureBox2.BackgroundImage = imageList1.Images[0];
+			pictureBox3.BackgroundImage = imageList1.Images[0];
+			pictureBox4.BackgroundImage = imageList1.Images[0];
+			pictureBox5.BackgroundImage = imageList1.Images[0];
+			pictureBox6.BackgroundImage = imageList1.Images[0];
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int[] random_groups = new int[6];
+			random_groups = GetRandom();
+			pictureBox1.BackgroundImage = imageList1.Images[random_groups[0]];
+			pictureBox2.BackgroundImage = imageList1.Images[random_groups[1]];
+			pictureBox3.BackgroundImage = imageList1.Images[random_groups[2]];
+			pictureBox4.BackgroundImage = imageList1.Images[random_groups[3]];
+			pictureBox5.BackgroundImage = imageList1.Images[random_groups[4]];
+			pictureBox6.BackgroundImage = imageList1.Images[random_groups[5]];
+		}
+		private int[] GetRandom()
+		{
+			int[] random_groups = new int[6];
+			for (int i = 0; i < random_groups.Length; i++)
+			{
+				Random rand = new Random(Guid.NewGuid().GetHashCode());
+				//ㄇㄉ一直中是怎樣 = = -->利用Guid.NewGuid()每一次所產生出來的結果都是不同的，再利用它產生雜湊碼來當成亂數產生器的種子，產生出真的很亂的亂數。
+				int temp = rand.Next(1, 42);//1<=random<10
+				Boolean check_duplicate = true;
+				while(check_duplicate)
+				{
+					if(!random_groups.Contains(temp))//check whether the number of temp is already contained in gruops
+					{
+						check_duplicate = false;
+						random_groups[i] = temp;
+					}
+					else
+					{
+						rand= new Random(Guid.NewGuid().GetHashCode());
+						temp = rand.Next(1, 42);
+					}
+				}
+			}
+			return random_groups;
+		}
+	}
+}
